81dc583ed2c9497b2885dca16452e233cd192d39
gusto <gustomize>
2020-08-11 17:38:46 -0300

    some configs

--- loja/src/main/java/br/com/alura/gusto/microservice/loja/service/CompraService.java
+++ loja/src/main/java/br/com/alura/gusto/microservice/loja/service/CompraService.java
@@ -5,6 +5,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
 import br.com.alura.gusto.microservice.loja.client.FornecedorClient;
 import br.com.alura.gusto.microservice.loja.controller.dto.CompraDto;
 import br.com.alura.gusto.microservice.loja.controller.dto.InfoFornecedorDto;
@@ -19,6 +21,7 @@ public class CompraService {
 	@Autowired
 	private FornecedorClient fornecedorClient;
 	
+	@HystrixCommand(fallbackMethod = "realizaCompraFallback")
 	public Compra realizaCompra(CompraDto compra) {
 		final String estado = compra.getEndereco().getEstado();
 		
@@ -36,4 +39,10 @@ public class CompraService {
 		return compraSalva;
 	}
 
+	public Compra realizaCompraFallback(CompraDto compra) {
+		Compra compraFallback = new Compra();
+		compraFallback.setEnderecoDestino(compra.getEndereco().getEstado());
+		return compraFallback;
+	}
+	
 }
